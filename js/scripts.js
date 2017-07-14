6f82601364bcce81ee701773f909eeeea306037b
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-14 15:40:26 -0700

    add function to display the results to the user in a list

--- js/scripts.js
+++ js/scripts.js
@@ -12,32 +12,22 @@ function pingProducer(userInput) {
       newArr.push(i);
     }
   }
-  // for (var x= 0; 0< newArr.length; i++){
-  //   document.write("<ul>" + "<li>" + newArr[x] + "</li>" + "</ul>")
-  //   alert
-  // }
-  // arrToList(newArr);
-  return newArr;
+  arrToList(newArr);
 }
-
-// function arrToList(newArr) {
-//   var list;
-//   var li;
-//   for (var x = newArr.length - 1; x >=0 ; x--) {
-//     list = document.getElementById('test');
-//     li = document.createElement('li');
-//     li.innerHTML = newArr[x];
-//     list.insertBefore(li, list.firstChild)
-//   }
-// }
+//enter elements into an ol list
+function arrToList(newArr) {
+  for (var x = newArr.length - 1; x >=0 ; x--) {
+    var list = document.getElementById('ulList');
+    var li = document.createElement('li');
+    li.innerHTML = newArr[x];
+    list.insertBefore(li, list.firstChild)
+  }
+}
 //User Interface
 $(document).ready(function(){
   onsubmit="setTimeout(function () { window.location.reload(); }, 10)"
   $("#gameForm").submit(function(event){
-
-    // document.getElementById("gameForm").onsubmit = function(){
-    //   location.reload(true);
-    // }
+    $("#ulList").empty();
     var input = $("input#number").val();
     var output = pingProducer(input);
     $("#pingPong").text(output);
